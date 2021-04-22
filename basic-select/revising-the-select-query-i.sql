4e7eaa5ae55703af31366d2fa4ace97141d2637a
Freddy-Liu <ronghanliufreddy@gmail.com>
2021-04-22 15:45:35 -0400

    Update revising-the-select-query-i.sql

--- basic-select/revising-the-select-query-i.sql
+++ basic-select/revising-the-select-query-i.sql
@@ -3,5 +3,5 @@
 -- https://www.hackerrank.com/challenges/revising-the-select-query/problem
 
 SELECT *
-FROM City
-WHERE CountryCode = 'USA' AND Population >= 100000;
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
